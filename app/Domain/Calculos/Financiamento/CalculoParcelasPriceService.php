140247964c2a2768f7c2e8a040d2cb44b0e32adc
Adriano Costa <adriano.dmcosta@gmail.com>
2023-05-28 13:59:55 -0300

    calcular juros parcelas price

--- app/Domain/Calculos/Financiamento/CalculoParcelasPriceService.php
+++ app/Domain/Calculos/Financiamento/CalculoParcelasPriceService.php
@@ -13,6 +13,7 @@ class CalculoParcelasPriceService implements CalculoParcelasServiceInterface
         for ($i = 1; $i <= $qtdParcelas; $i++) {
             $parcelas[] = [
                 'numero' => $i,
+                'valorJuros' => $this->calcularJurosParcela($valorTotal, $qtdParcelas, $taxaMensal, $i),
                 'valorPrestacao' => $valorPrestacao,
             ];
         }
@@ -33,11 +34,40 @@ class CalculoParcelasPriceService implements CalculoParcelasServiceInterface
 
     private function calculaFatorPrice(Decimal $taxaMensal, int $qtdParcelas): Decimal
     {
-        return ((1 + $taxaMensal) ** $qtdParcelas) / (((1 + $taxaMensal) ** $qtdParcelas) - 1);
+        return ((1 + $taxaMensal) ** $qtdParcelas)
+            / (((1 + $taxaMensal) ** $qtdParcelas) - 1);
     }
 
-    public function arrendondarValor(Decimal $valor, int $casasDecimais = 2): Decimal
+    private function arrendondarValor(Decimal $valor, int $casasDecimais = 2): Decimal
     {
         return $valor->round(2, Decimal::ROUND_HALF_EVEN);
     }
+
+    /**
+     * Calcula o valor dos juros de uma parcela no sistema de amortização Price
+     */
+    private function calcularJurosParcela(Decimal $valorTotal, int $qtdParcelas, Decimal $taxaMensal, int $numeroParcela): Decimal
+    {
+        throw_if(($numeroParcela < 1 || $numeroParcela > $qtdParcelas), new \InvalidArgumentException('Número da parcela inválido'));
+
+        $valorParcela = $this->calcularValorPrestacao($valorTotal, $qtdParcelas, $taxaMensal);
+        $saldoDevedor = $this->calcularSaldoDevedor($valorTotal, $taxaMensal, $numeroParcela, $valorParcela);
+        $jurosParcela = $this->calcularJuros($saldoDevedor, $taxaMensal);
+
+        return $this->arrendondarValor($jurosParcela);
+    }
+
+    private function calcularSaldoDevedor(Decimal $valorTotal, Decimal $taxaMensal, int $numeroParcela, Decimal $valorParcela): Decimal
+    {
+
+        return $valorTotal * ((1 + $taxaMensal) ** ($numeroParcela - 1))
+            - $valorParcela * (((1 + $taxaMensal) ** ($numeroParcela - 1)) - 1) / $taxaMensal;
+
+    }
+
+    private function calcularJuros(Decimal $saldoDevedor, Decimal $taxaMensal): Decimal
+    {
+        return $saldoDevedor * $taxaMensal;
+    }
+
 }
--- tests/Feature/Domain/Calculos/Financiamento/CalculoParcelasPriceServiceTest.php
+++ tests/Feature/Domain/Calculos/Financiamento/CalculoParcelasPriceServiceTest.php
@@ -7,14 +7,35 @@ use Tests\TestCase;
 
 class CalculoParcelasPriceServiceTest extends TestCase
 {
-    public function test_calculo_de_prestação_price(): void
+    public function test_gerar_numero_prestacoes_price(): void
     {
         $service = app()->make('App\Domain\Calculos\Financiamento\CalculoParcelasPriceService');
         $parcelas = $service->calcularParcelas(new Decimal('900', 2), 5, new Decimal('0.0179', 9));
         $this->assertEquals(5, count($parcelas));
-        // dd($parcelas[0]['valorPrestacao']->tostring(), (new Decimal('173.67', 18))->tostring(), ($parcelas[0]['valorAmortizacao'])->tostring());
+        for ($i = 0; $i < 5; $i++) {
+            $this->assertEquals($i + 1, $parcelas[$i]['numero']);
+        }
+    }
+
+    public function test_calculo_valor_prestacoes_price(): void
+    {
+        $service = app()->make('App\Domain\Calculos\Financiamento\CalculoParcelasPriceService');
+        $parcelas = $service->calcularParcelas(new Decimal('900', 2), 5, new Decimal('0.0179', 9));
         for ($i = 0; $i < 5; $i++) {
             $this->assertEquals('189.78', $parcelas[$i]['valorPrestacao']->tostring());
         }
     }
+
+    public function test_calculo_valor_juros_price(): void
+    {
+        $service = app()->make('App\Domain\Calculos\Financiamento\CalculoParcelasPriceService');
+        $parcelas = $service->calcularParcelas(new Decimal('900', 2), 5, new Decimal('0.0179', 9));
+
+        $this->assertEquals('16.11', $parcelas[0]['valorJuros']->tostring());
+        $this->assertEquals('13.00', $parcelas[1]['valorJuros']->tostring());
+        $this->assertEquals('9.84', $parcelas[2]['valorJuros']->tostring());
+        $this->assertEquals('6.62', $parcelas[3]['valorJuros']->tostring());
+        $this->assertEquals('3.34', $parcelas[4]['valorJuros']->tostring());
+    }
+
 }
