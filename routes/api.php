f6207ea9fe3a825bf1a5288cd45e446c90aa434a
Adriano Costa <adriano.dmcosta@gmail.com>
2023-06-10 16:11:46 -0300

    rota para testar se a simulação foi enviada para o eventhub

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,46 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/', SimulacaoController::class);
+
+Route::get('/simulacoes', function () {
+    $conf = new \RdKafka\Conf();
+    $conf->set('group.id', 'hack');
+    $conf->set('metadata.broker.list', 'eventhack.servicebus.windows.net:9093');
+    $conf->set('security.protocol', 'SASL_SSL');
+    $conf->set('sasl.mechanisms', 'PLAIN');
+    $conf->set('sasl.username', '$ConnectionString'); // exactly as you see it
+    $conf->set('sasl.password', 'Endpoint=sb://eventhack.servicebus.windows.net/;SharedAccessKeyName=hack;SharedAccessKey=HeHeVaVqyVkntO2FnjQcs2Ilh/4MUDo4y+AEhKp8z+g=;EntityPath=simulacoes'); // should look like: Endpoint=sb://sp-event-hubs.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=someStringHere
+    $conf->set('enable.partition.eof', 'true');
+    // $conf->set('api.version.request', 'false');
+    // $conf->set('log_level', (string) LOG_DEBUG);
+    // $conf->set('debug', 'all');
+    $conf->set('auto.offset.reset', 'latest');
+
+    $consumer = new RdKafka\KafkaConsumer($conf);
+
+    // Subscribe to topic 'test'
+    $consumer->subscribe(['simulacoes']);
+    while (true) {
+        $message = $consumer->consume(60 * 1000);
+        switch ($message->err) {
+            case RD_KAFKA_RESP_ERR_NO_ERROR:
+                var_dump($message);
+                break;
+            case RD_KAFKA_RESP_ERR__PARTITION_EOF:
+                echo "No more messages; will wait for more\n";
+                exit();
+                break;
+            case RD_KAFKA_RESP_ERR__TIMED_OUT:
+                echo "Timed out\n";
+                exit();
+                break;
+            default:
+                throw new \Exception($message->errstr(), $message->err);
+                break;
+        }
+    }
+
+    $producer = new \RdKafka\Producer($conf);
+    $producer->purge(RD_KAFKA_PURGE_F_QUEUE);
+    $result = $producer->flush(10000);
+});
