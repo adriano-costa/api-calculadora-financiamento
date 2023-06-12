2a967083fdd7b898cb988b4bda52b3fff897b061
ADRIANO DE MELO COSTA <adriano.m.costa@caixa.gov.br>
2023-06-12 13:31:03 -0300

    incluir valores padrões para as propriedades eventhub

--- config/eventhub.php
+++ config/eventhub.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'sas_key_name' => env('EVENT_HUB_SAS_KEY_NAME'),
-    'sas_key' => env('EVENT_HUB_SAS_KEY'),
-    'host' => env('EVENT_HUB_HOST'),
-    'entity_path' => env('EVENT_HUB_ENTITY_PATH'),
+    'sas_key_name' => env('EVENT_HUB_SAS_KEY_NAME', 'hack'),
+    'sas_key' => env('EVENT_HUB_SAS_KEY', 'xxxxxxxx'),
+    'host' => env('EVENT_HUB_HOST', 'hack.servicebus.windows.net'),
+    'entity_path' => env('EVENT_HUB_ENTITY_PATH', 'simulacoes'),
     'validar_certificado_ssl' => env('EVENT_HUB_VALIDAR_CERTIFICADO_SSL', true),
 ];
